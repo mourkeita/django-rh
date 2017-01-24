1c4d78f0e019a8fb9d1b06452bbb38c0323e57ab
Mour KEITA <mour.keita@gmail.com>
2017-01-24 23:16:28 +0100

    Interface update

--- rh/qa/forms.py
+++ rh/qa/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Qa
 
 class QaForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput)
 	class Meta:
 		model = Qa
 		fields = ('first','last','email','password', 'age')
--- rh/qa/models.py
+++ rh/qa/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Qa(models.Model):
     first = models.CharField(max_length=200) 
     last = models.CharField(max_length=200)
-    email = models.EmailField(max_length=100, blank=True, null= True, unique= True, default="Email")
-    password = models.CharField(max_length=100, default="Mot de passe")
+    email = models.EmailField(max_length=100, blank=True, null= True, unique= True, default="")
+    password = models.CharField(max_length=100, default="")
     age = models.IntegerField()
