0b9cae76b251f7df4718c430b36d0e8a9941967b
Mour KEITA <mour.keita@gmail.com>
2017-01-31 01:45:56 +0100

    Update

--- rh/qa/views.py
+++ rh/qa/views.py
@@ -14,14 +14,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h3>Hello, world. Welcome to RH QA app.</h3><p><h4>C'est quoi les objectifs de la QA ? </h4>")
-
-def logout(request):
-    return render_to_response('logout.html')
-
-def welcome(request):
-    return render_to_response('welcome.html')
-
+    return HttpResponseRedirect("/login")
 
 def listUsers(request):
     users = Qa.objects.all().order_by('id')
@@ -36,25 +29,6 @@ def listUsers(request):
 def texte(request):
      HttpResponse("<h3>Hello this is a test</h3>")
 
-
-@csrf_exempt 
-def login(request):
-    if len(request.POST) > 0:
-        if 'email' not in request.POST or 'password' not in request.POST:
-            error = 'Veuillez entrer un email et un mot de passe'
-            return render_to_response('login.html', {'error':error})
-        else:
-            email = request.POST['email']
-            password = request.POST['password']
-            result = Qa.objects.filter(email=email).first()
-            if email != result.email or password != result.password:
-                error = u'Mot de passe ou email errone'
-		return render_to_response('login.html', {'error':error})
-            else:
-               return HttpResponseRedirect('/welcome')
-    else:
-        return render_to_response('login.html')
-
 def newuser(request):
     form = QaForm()
     return render(request, 'new_user.html', {'form':form})
@@ -68,3 +42,28 @@ def userdetails(request):
         return render(request, 'user_details.html', {'post':post} )
     else:
         return HttpResponseRedirect('/newuser')
+
+def logout(request):
+    return render_to_response('logout.html')
+
+def welcome(request):
+    return render_to_response('welcome.html')
+
+@csrf_exempt 
+def login(request):
+    username = "not logged in"
+    if len(request.POST) > 0:
+        if 'email' not in request.POST or 'password' not in request.POST:
+            error = 'Veuillez entrer un email et un mot de passe'
+            return render_to_response('login.html', {'error':error})
+        else:
+            email = request.POST['email']
+            password = request.POST['password']
+            result = Qa.objects.filter(email=email).first()
+            if email != result.email or password != result.password:
+                error = u'Mot de passe ou email errone'
+                return render_to_response('login.html', {'error':error})
+            else:
+               return HttpResponseRedirect('/welcome')
+    else:
+        return render_to_response('login.html')
