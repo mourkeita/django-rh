39e106854823cce1472066f6f1cf031833045c51
mourkeita <mour.keita@gmail.com>
2020-11-02 00:18:41 +0100

    Remove wsgiref package Remove social media auth

--- rh/rh/settings.py
+++ rh/rh/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'social.apps.django_app.default',
+                #'social.apps.django_app.default',
                 'social_django.context_processors.backends',  # <--
                 'social_django.context_processors.login_redirect', # <--
             ],
